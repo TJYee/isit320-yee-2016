8b8d88f48287b492c7d4a39ceb82868403867505
Taylor School VBox <TaylorYeeBC@gmail.com>
2016-10-03 17:21:24 -0700

    Commit Week03-CouchDemo project

---
+++ Week03-CouchDemo/server.js
@@ -0,0 +1,34 @@
+var nano = require('nano')('http://168.156.44.102:5984');
+// var nano = require('nano')('http://168.156.44.102:5984');
+
+var docName = 'bigNames';
+var dbName = 'bc_data';
+
+var readIt = function() {
+	var prog = nano.db.use(dbName);
+	prog.get(docName, { revs_info: true }, function(err, body) {
+		if (!err)
+			console.log(body);
+	});
+}
+
+function insert() {
+	nano.db.create(dbName);
+	var prog = nano.db.use(dbName);
+
+	prog.insert({"doc":[
+			{ 'firstName': 'Suzie', 'lastName': 'Higgins'},
+			{ 'firstName': 'Bob', 'lastName': 'Ross'},
+			{ 'firstName': 'John', 'lastName': 'Doe'},
+			{ 'firstName': 'John', 'lastName': 'Johnson'}
+			]},
+			docName, function(err, body) {
+		if (!err)
+			console.log(body);
+		readIt();
+	});
+}
+
+
+insert();
+// readIt();
