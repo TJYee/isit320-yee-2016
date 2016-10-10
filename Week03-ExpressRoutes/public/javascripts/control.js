1f755e1e770ae91f1cba98ebfedb56302b5ebcc1
Taylor Home VBox <TaylorYeeBC@gmail.com>
2016-10-09 21:30:30 -0700

    Updated Week03-ExpressRoutes

---
+++ Week03-ExpressRoutes/public/javascripts/control.js
@@ -0,0 +1,42 @@
+/**
+ * Created by bcuser on 10/9/16.
+ */
+
+$(document).ready(function() {
+    'use strict';
+    console.log('Document loaded in prog272');
+
+    $('#read').click(read);
+    $('#readJson').click(callReadJson);
+    $('#add').click(add);
+
+    function read() {
+        console.log('callRead called');
+        $.getJSON('/read', function(result) {
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+        });
+    }
+
+    function callReadJson() {
+        console.log('readJson called');
+        $.getJSON('names.json', function(result) {
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+        });
+    }
+
+    function add() {
+        var operatorA = $('#operatorA').val();
+        var operatorB = $('#operatorB').val();
+        console.log('operators:', operatorA, operatorB);
+        var requestQuery = {
+            operatorA: operatorA,
+            operatorB: operatorB
+        };
+        $.getJSON('/add', requestQuery, function(result) {
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+        });
+    }
+});
--- Week03-ExpressRoutes/routes/index.js
+++ Week03-ExpressRoutes/routes/index.js
@@ -3,14 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yee' });
+    'use strict';
+    res.render('index', {
+        title: 'Yee'
+    });
 });
 
 router.get('/read', function(request, response) {
-  response.send([
-    {name: 'SarahLee'},
-    {name: 'Bob'}
-  ]);
+    'use strict';
+    console.log('read mehtod called');
+    response.send([{
+        name: 'SarahLee'
+    }, {
+        name: 'Bob'
+    }]);
+});
+
+router.get('/add', function(request, response) {
+    'use strict';
+    console.log('add method called');
+    console.log('The parameters are:', request.query);
+    var result = parseInt(request.query.operatorA) + parseInt(request.query.operatorB);
+    response.send([
+        result
+    ]);
 });
 
 module.exports = router;
