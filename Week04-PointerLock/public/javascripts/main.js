2461ce8f045269644bf822fb29f23fd5cd1cc707
Taylor School VBox <TaylorYeeBC@gmail.com>
2016-10-17 15:42:19 -0700

    Updating Week04-PointerLock

--- Week04-PointerLock/public/javascripts/main.js
+++ Week04-PointerLock/public/javascripts/main.js
@@ -2,7 +2,6 @@ requirejs.config({
     baseUrl: '.',
     paths: {
         'jquery': 'components/jquery/dist/jquery',
-        'bootstrap': 'components/bootstrap/dist/js/bootstrap',
         'Three': 'javascripts/three',
         'control': 'javascripts/control',
         'floor': 'javascripts/floor',
@@ -18,7 +17,7 @@ requirejs.config({
 
 requirejs(['jquery'], function() {
     'use strict';
-    requirejs(['bootstrap', 'Three', 'control'], function(bootstrap, THREE, Control) {
+    requirejs(['Three', 'control'], function(THREE, Control) {
         $(document).ready(function() {
             var control = new Control(THREE);
         });
