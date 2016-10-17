c9d0e22ded39553cc67b1189ef58c8628c4eaed7
Taylor Home VBox <TaylorYeeBC@gmail.com>
2016-10-17 01:11:30 -0700

    Updating Week04-ClickRoute project

--- Week04-ClickRoute/public/javascripts/ClickEvents.js
+++ Week04-ClickRoute/public/javascripts/ClickEvents.js
@@ -4,30 +4,30 @@ define(['jquery'], function(jquery) {
     var elf = {};
     elf.run = {};
 
-    elf.ClickEvents = ( function() {
-            var listItem = $('.listItem');
-            var intro = $('#intro');
+    elf.ClickEvents = (function() {
+        var listItem = $('.listItem');
+        var intro = $('#intro');
 
-            function ClickEvents() {
-                $(intro).html('ClickEvents is loaded. Click the three items seen below.');
-                $(intro).addClass('blue');
-                $(listItem).click(listClick);                
-            }
+        function ClickEvents() {
+            $(intro).html('ClickEvents is loaded. Click the three items seen below.');
+            $(intro).addClass('blue');
+            $(listItem).click(listClick);
+        }
 
-            var listClick = function(event) {
-                var clickText = event.target.innerText;
-                var prompt = 'You clicked: ';
-                $(intro).html(prompt + clickText);
-                var routeCall = '/' + clickText;
-                $.getJSON(routeCall, function(response) {
-                    $(result).html(response.result);
-                    $(route).html(response.route);
-                    $(message).html(response.message);
-                });
-            };
+        var listClick = function(event) {
+            var clickText = event.target.innerText;
+            var prompt = 'You clicked: ';
+            $(intro).html(prompt + clickText);
+            var routeCall = '/' + clickText;
+            $.getJSON(routeCall, function(response) {
+                $(result).html(response.result);
+                $(route).html(response.route);
+                $(message).html(response.message);
+            });
+        };
 
-            return ClickEvents;
-        }());
+        return ClickEvents;
+    }());
 
     return elf.ClickEvents;
 
--- Week04-ClickRoute/routes/index.js
+++ Week04-ClickRoute/routes/index.js
@@ -3,27 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(request, response) {
-  response.render('index', { title: 'Click Routes' });
+    'use strict';
+    response.render('index', {
+        title: 'Click Routes'
+    });
 });
 
 router.get('/Item01', function(request, response) {
-    var result = { 'result': 'Success',
+    'use strict';
+    var result = {
+        'result': 'Success',
         'route': '/Item01',
-        'message': 'The server sent me.'};
+        'message': 'The server sent me.'
+    };
     response.send(result);
 });
 
 router.get('/Item02', function(request, response) {
-    var result = { 'result': 'Success',
+    'use strict';
+    var result = {
+        'result': 'Success',
         'route': '/Item02',
-        'message': 'The server sent me.'};
+        'message': 'The server sent me.'
+    };
     response.send(result);
 });
 
 router.get('/Item03', function(request, response) {
-    var result = { 'result': 'Success',
+    'use strict';
+    var result = {
+        'result': 'Success',
         'route': '/Item03',
-        'message': 'The server sent me.'};
+        'message': 'The server sent me.'
+    };
     response.send(result);
 });
 
