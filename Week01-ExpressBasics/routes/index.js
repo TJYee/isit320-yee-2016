efc46dcddbc0adb531d925e0c0644beee51771a0
Taylor Home VBox <TaylorYeeBC@gmail.com>
2016-10-21 21:27:39 -0700

    Committing SmokeTest file

--- Week01-ExpressBasics/routes/index.js
+++ Week01-ExpressBasics/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ISIT320' });
+    'use strict';
+    res.render('index', {
+        title: 'ISIT320'
+    });
 });
 
 module.exports = router;
