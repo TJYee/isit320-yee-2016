d271d30a697bd71104a382ce63319565ea32a350
Taylor School Laptop <tayloryeebc@gmail.com>
2016-12-12 18:55:15 -0800

    Testing

--- DataMaster/routes/CouchBulk.js
+++ DataMaster/routes/CouchBulk.js
@@ -41,7 +41,8 @@ function couchBulk(router, dbName, server) {
 
     router.get('/insertBulk', function(request, response) {
         console.log('bulk data');
-        var fileName = '../' + request.query.fileName;
+        //var fileName = request.query.fileName;
+        var fileName = '~/bin/datamasters/Npcs.json'
         var record = fs.readFile(fileName, 'utf8', function(err, json) {
             console.log('Reading file');
             console.log(json);
